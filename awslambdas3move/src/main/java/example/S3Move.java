ced215288a376b7ac4a1ed3082c148fca649fcc9
alfonsof <alfonsofb@gmail.com>
2018-02-16 16:19:13 +0100

    Update comments S3Move.java

--- awslambdas3move/src/main/java/example/S3Move.java
+++ awslambdas3move/src/main/java/example/S3Move.java
@@ -1,6 +1,7 @@
 /**
- * Example for using a AWS Lambda Function
- * Move a file when it appears in a S3 bucket to another S3 bucket
+ * AWS Lambda Function S3 Move Java example
+ * It handles an AWS Lambda function that moves a file
+ * when it appears in a S3 bucket to another S3 bucket
  */
 
 package example;
