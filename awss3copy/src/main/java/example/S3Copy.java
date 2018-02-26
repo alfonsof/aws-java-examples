5a7c365437479f6bf0df3b472aaff372613dc520
alfonsof <alfonsofb@gmail.com>
2018-02-26 14:29:00 +0100

    Add cmd parameter

--- awss3copy/src/main/java/example/S3Copy.java
+++ awss3copy/src/main/java/example/S3Copy.java
@@ -1,6 +1,10 @@
 /**
  * S3Copy is an example that handles S3 buckets on AWS
  * Copy a file from a S3 bucket to another S3 bucket
+ * You must use 3 parameters:
+ * SOURCE_BUCKET      = Source bucket name
+ * SOURCE_FILE        = Source file name
+ * DESTINATION_BUCKET = Destination bucket name
  */
 
 package example;
@@ -13,24 +17,37 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.CopyObjectRequest;
 
 public class S3Copy {
-    private static String SourceBucketName      = "sourcevm";  // Source bucket name
-    private static String SourceKey             = "example.txt";  // Source key
-    private static String DestinationBucketName = "targetvm";  // Destination bucket name
-    private static String destinationKey        = "example.txt";  // Destination key
 
     public static void main(String[] args) throws IOException {
-        AmazonS3 s3client = AmazonS3ClientBuilder.defaultClient();
-        try {
-            System.out.println("From - bucket: " + SourceBucketName);
-            System.out.println("From - file:   "   + SourceKey);
-            System.out.println("To   - bucket: " + DestinationBucketName);
-            System.out.println("To   - file:   "   + destinationKey);
+        String sourceBucketName;       // Source bucket name
+        String sourceKey;              // Source key
+        String destinationBucketName;  // Destination bucket name
+        String destinationKey;         // Destination key
 
+        if (args.length < 3) {
+            System.out.println("Not enough parameters. Proper Usage is: java -jar s3copy.jar <SOURCE_BUCKET> <SOURCE_FILE> <DESTINATION_BUCKET>");
+            System.exit(1);
+        }
+
+        sourceBucketName      = args[0];
+        sourceKey             = args[1];
+        destinationBucketName = args[2];
+        destinationKey        = sourceKey;
+
+        System.out.println("From - bucket: " + sourceBucketName);
+        System.out.println("From - file:   " + sourceKey);
+        System.out.println("To   - bucket: " + destinationBucketName);
+        System.out.println("To   - file:   " + destinationKey);
+
+        // Instantiates a client
+        AmazonS3 s3client = AmazonS3ClientBuilder.defaultClient();
+
+        try {
             System.out.println("Copying object...");
 
             // Copy object
             CopyObjectRequest copyObjRequest = new CopyObjectRequest(
-                    SourceBucketName, SourceKey, DestinationBucketName, destinationKey);
+                    sourceBucketName, sourceKey, destinationBucketName, destinationKey);
             s3client.copyObject(copyObjRequest);
 
             System.out.println("Copied");
