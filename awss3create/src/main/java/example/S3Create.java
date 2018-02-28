c37b196c0452afa03339840ecac7c0939d6e9d0d
alfonsof <alfonsofb@gmail.com>
2018-02-28 16:08:39 +0100

    Update comments

--- awss3create/src/main/java/example/S3Create.java
+++ awss3create/src/main/java/example/S3Create.java
@@ -1,7 +1,7 @@
 /**
  * S3Create is an example that handles S3 buckets on AWS
  * Create a new S3 bucket
- * You must use 1 parameter:
+ * You must provide 1 parameter:
  * BUCKET_NAME = Name of the bucket
  */
 
