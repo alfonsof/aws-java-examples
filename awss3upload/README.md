b0408f2801b145f68923792922b82ea15ac8112d
alfonsof <alfonsofb@gmail.com>
2018-02-28 12:15:20 +0100

    First commit

---
+++ awss3upload/README.md
@@ -0,0 +1,37 @@
+# AWS S3 Upload Java example
+
+This folder contains a Java application example that handles S3 buckets on AWS (Amazon Web Services).
+
+Upload a local file to a S3 bucket.
+
+
+
+
+## Requirements
+
+* You must have an [Amazon Web Services (AWS) account](http://aws.amazon.com/).
+
+This code was written for Java 1.8 and AWS SDK for Java 1.11.x.
+
+
+
+
+## Using the code
+
+* Configure your AWS access keys.
+
+* Run the code:
+
+  You must provide 3 parameters:
+  
+  <BUCKET_NAME>     = Bucket name
+  <OBJECT_NAME>     = Object file name in the bucket
+  <LOCAL_FILE_NAME> = Local file name
+
+  ```
+  java -jar awss3upload.jar bucket-name object-name local-file
+  ```
+
+* Test the application:
+
+  You should see the new object created in the S3 bucket.
---
+++ awss3upload/src/main/java/example/S3Upload.java
@@ -0,0 +1,74 @@
+/**
+ * S3Upload is an example that handles S3 buckets on AWS
+ * Upload a local file to a S3 bucket
+ * SOURCE_BUCKET   = Source bucket name
+ * OBJECT_NAME     = Object file name in the bucket
+ * LOCAL_FILE_NAME = Local file name
+ */
+
+package example;
+
+import java.io.File;
+import java.io.IOException;
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+
+public class S3Upload {
+    public static void main(String[] args) throws IOException {
+        String bucketName;     // Source bucket name
+        String keyName;        // Key name, it is the object name
+        String uploadFileName; // Upload local file name
+
+        if (args.length < 3) {
+            System.out.println("Not enough parameters. Proper Usage is: java -jar s3upload.jar <SOURCE_BUCKET> <OBJECT_NAME> <LOCAL_FILE_NAME>");
+            System.exit(1);
+        }
+
+        bucketName     = args[0];
+        keyName        = args[1];
+        uploadFileName = args[2];
+
+        System.out.println("Bucket:     " + bucketName);
+        System.out.println("Object/Key: " + keyName);
+        System.out.println("Local file: " + uploadFileName);
+
+        // Instantiates a client
+        AmazonS3 s3client = AmazonS3ClientBuilder.defaultClient();
+
+        try {
+            System.out.println("Uploading an object to S3 from a file ...");
+
+            // Get local file
+            File file = new File(uploadFileName);
+
+            // Upload object
+            s3client.putObject(new PutObjectRequest(
+                    bucketName, keyName, file));
+            
+            System.out.println("Uploaded");
+
+        } catch (AmazonServiceException ase) {
+            System.out.println("Caught an AmazonServiceException, " +
+                    "which means your request made it " +
+                    "to Amazon S3, but was rejected with an error " +
+                    "response for some reason.");
+            System.out.println("Error Message:    " + ase.getMessage());
+            System.out.println("HTTP Status Code: " + ase.getStatusCode());
+            System.out.println("AWS Error Code:   " + ase.getErrorCode());
+            System.out.println("Error Type:       " + ase.getErrorType());
+            System.out.println("Request ID:       " + ase.getRequestId());
+        } catch (AmazonClientException ace) {
+            System.out.println("Caught an AmazonClientException, " +
+                    "which means the client encountered " +
+                    "an internal error while trying to " +
+                    " communicate with S3, " +
+                    "such as not being able to access the network.");
+            System.out.println("Error Message: " + ace.getMessage());
+        }
+        s3client.shutdown();
+    }
+
+}
