d716abc3c3da2c9312f51b4a770257cf780db7e1
alfonsof <alfonsofb@gmail.com>
2018-02-28 11:08:06 +0100

    Fist commit

---
+++ awss3listall/README.md
@@ -0,0 +1,35 @@
+# AWS S3 List All Java example
+
+This folder contains a Java application example that handles S3 buckets on AWS (Amazon Web Services).
+
+List information about all S3 buckets and the files they contain for.
+
+
+
+
+## Requirements
+
+* You must have an [Amazon Web Services (AWS) account](http://aws.amazon.com/).
+
+This code was written for Java 1.8 and AWS SDK for Java 1.11.x.
+
+
+
+
+## Using the code
+
+* Configure your AWS access keys.
+
+* Create some S3 buckets.
+
+* Copy some files to the S3 buckets.
+
+* Run the code:
+
+  ```
+  java -jar awss3list.jar bucket-name
+  ```
+
+* Test the application:
+
+  You should see the list of buckets and files stored in the S3 buckets.
---
+++ awss3listall/src/main/java/example/S3ListAll.java
@@ -0,0 +1,60 @@
+/**
+ * S3ListAll is an example that handles S3 buckets on AWS
+ * List information about all S3 buckets and the files they contain
+ */
+
+package example;
+
+import java.io.IOException;
+import java.util.List;
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.Bucket;
+import com.amazonaws.services.s3.model.ListObjectsRequest;
+import com.amazonaws.services.s3.model.ObjectListing;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+
+public class S3ListAll {
+    public static void main(String[] args) throws IOException {
+
+        // Instantiates a client
+        AmazonS3 s3client = AmazonS3ClientBuilder.defaultClient();
+
+        try {
+            System.out.println("Listing S3 buckets and objects ...");
+
+            List<Bucket> buckets = s3client.listBuckets();
+            System.out.println("Your Amazon S3 buckets are:");
+            String bucketName;
+            for (Bucket b : buckets) {
+                bucketName = b.getName();
+                System.out.println("* Bucket: " + bucketName);
+                ObjectListing objectListing = s3client.listObjects(new ListObjectsRequest()
+                        .withBucketName(bucketName));
+                for (S3ObjectSummary objectSummary : objectListing.getObjectSummaries()) {
+                    System.out.println(" - Object: " + objectSummary.getKey() + "  " +
+                            "(size = " + objectSummary.getSize() + ")");
+                }
+            }
+        } catch (AmazonServiceException ase) {
+            System.out.println("Caught an AmazonServiceException, " +
+                    "which means your request made it " +
+                    "to Amazon S3, but was rejected with an error response " +
+                    "for some reason.");
+            System.out.println("Error Message:    " + ase.getMessage());
+            System.out.println("HTTP Status Code: " + ase.getStatusCode());
+            System.out.println("AWS Error Code:   " + ase.getErrorCode());
+            System.out.println("Error Type:       " + ase.getErrorType());
+            System.out.println("Request ID:       " + ase.getRequestId());
+        } catch (AmazonClientException ace) {
+            System.out.println("Caught an AmazonClientException, " +
+                    "which means the client encountered " +
+                    "an internal error while trying to communicate" +
+                    " with S3, " +
+                    "such as not being able to access the network.");
+            System.out.println("Error Message: " + ace.getMessage());
+        }
+    }
+}
