31828f089d52a3516c9db57d31e4c971e8911bc2
alfonsof <alfonsofb@gmail.com>
2018-02-28 16:22:20 +0100

    Add some spaces

--- awslambdas3example/src/main/java/example/S3Example.java
+++ awslambdas3example/src/main/java/example/S3Example.java
@@ -11,9 +11,9 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.S3Event;
 import com.amazonaws.services.s3.event.S3EventNotification.S3EventNotificationRecord;
 
-public class S3Example implements RequestHandler<S3Event, String>{
+public class S3Example implements RequestHandler<S3Event, String> {
 
-    public String handleRequest(S3Event input, Context context){
+    public String handleRequest(S3Event input, Context context) {
         // Get Event Record
         S3EventNotificationRecord record = input.getRecords().get(0);
 
@@ -23,9 +23,9 @@ public class S3Example implements RequestHandler<S3Event, String>{
         // Source File Name
         String srcFileName = record.getS3().getObject().getKey(); // Name doesn't contain any special characters
 
-        context.getLogger().log("Input: " + input + "\n");
+        context.getLogger().log("Input:  " + input + "\n");
         context.getLogger().log("Bucket: " + srcBucketName + "\n");
-        context.getLogger().log("File: " + srcFileName + "\n");
+        context.getLogger().log("File:   " + srcFileName + "\n");
 
         return srcBucketName + "/" + srcFileName;
     }
