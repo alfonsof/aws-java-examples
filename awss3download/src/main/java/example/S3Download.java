30bf7200aa6c3dd5dcc6b6d654a5f156c89cabb7
alfonsof <alfonsofb@gmail.com>
2018-09-04 10:26:28 +0200

    Updated print messages

--- awss3download/src/main/java/example/S3Download.java
+++ awss3download/src/main/java/example/S3Download.java
@@ -1,6 +1,6 @@
 /**
- * S3Download is an example that handles S3 buckets on AWS
- * Download an object from a S3 bucket to a local file
+ * S3Download is an example that handles S3 buckets on AWS.
+ * Download an object from a S3 bucket to a local file.
  * You must provide 3 parameters:
  * BUCKET_NAME     = Bucket name
  * OBJECT_NAME     = Object file name in the bucket
@@ -24,25 +24,27 @@ public class S3Download {
     private static final int MAX_BUFFER_SIZE = 1024*1000;  // Maximum buffer size for the file
 
     public static void main(String[] args) throws IOException {
-        String bucketName;         // Source bucket name
-        String keyName;            // Key name, it is the object name
-        String downloadFileName;   // Upload local file name
+        String region = "eu-west-1";  // Region name
+        String bucketName;            // Source bucket name
+        String keyName;               // Key name, it is the object name
+        String localFileName;         // Local file name
 
         if (args.length < 3) {
-            System.out.println("Not enough parameters. Proper Usage is: java -jar s3download.jar <BUCKET_NAME> <OBJECT_NAME> <LOCAL_FILE_NAME>");
+            System.out.println("Not enough parameters.\nProper Usage is: java -jar s3download.jar <BUCKET_NAME> <OBJECT_NAME> <LOCAL_FILE_NAME>");
             System.exit(1);
         }
 
-        bucketName       = args[0];
-        keyName          = args[1];
-        downloadFileName = args[2];
+        bucketName    = args[0];
+        keyName       = args[1];
+        localFileName = args[2];
 
         System.out.println("Bucket:     " + bucketName);
         System.out.println("Object/Key: " + keyName);
-        System.out.println("Local file: " + downloadFileName);
+        System.out.println("Local file: " + localFileName);
 
         // Instantiates a client
-        AmazonS3 s3client = AmazonS3ClientBuilder.defaultClient();
+        AmazonS3 s3client = AmazonS3ClientBuilder.standard()
+                .withRegion(region).build();
 
         try {
             System.out.println("Downloading an object from a S3 to a local file ...");
@@ -54,19 +56,15 @@ public class S3Download {
             S3ObjectInputStream inputStream = s3object.getObjectContent();
 
             // Write content to the file
-            FileOutputStream fileOutputStream = new FileOutputStream(new File(downloadFileName));
+            FileOutputStream fileOutputStream = new FileOutputStream(new File(localFileName));
             byte[] readBuffer = new byte[MAX_BUFFER_SIZE];
             int readLen = 0;
             while ((readLen = inputStream.read(readBuffer)) > 0) {
                 fileOutputStream.write(readBuffer, 0, readLen);
             }
             fileOutputStream.close();
-
             inputStream.close();
-
-
             System.out.println("Downloaded");
-
         } catch (AmazonServiceException ase) {
             System.out.println("Caught an AmazonServiceException, " +
                     "which means your request made it " +
