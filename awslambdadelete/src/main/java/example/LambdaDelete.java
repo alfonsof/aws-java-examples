bf590ee07593a2217c884d5272e26e60266ad5fb
alfonsof <alfonsofb@gmail.com>
2020-08-06 20:27:40 +0200

    First commit

---
+++ awslambdadelete/src/main/java/example/LambdaDelete.java
@@ -0,0 +1,68 @@
+/**
+ * LambdaDelete is an example that handles Lambda functions on AWS.
+ * Delete a Lambda function.
+ * You must provide 1 parameter:
+ * FUNCTION_NAME      = Lambda function name
+ */
+
+package example;
+
+import java.io.IOException;
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.lambda.AWSLambda;
+import com.amazonaws.services.lambda.AWSLambdaClientBuilder;
+import com.amazonaws.services.lambda.model.ServiceException;
+import com.amazonaws.services.lambda.model.DeleteFunctionRequest;
+
+
+public class LambdaDelete {
+
+    private static final String REGION = "eu-west-1";      // Region name
+
+    public static void main(String[] args) throws IOException {
+
+        if (args.length < 1) {
+            System.out.println("Not enough parameters.\nProper Usage is: java -jar lambdadelete.jar <FUNCTION_NAME>");
+            System.exit(1);
+        }
+
+        String functionName = args[0];
+
+        System.out.println("Lambda function name: " + functionName);
+
+        try {
+            AWSLambda awsLambda = AWSLambdaClientBuilder.standard()
+                    .withCredentials(new ProfileCredentialsProvider())
+                    .withRegion(REGION).build();
+
+            DeleteFunctionRequest delFunc = new DeleteFunctionRequest();
+            delFunc.withFunctionName(functionName);
+
+            //Delete the function
+            awsLambda.deleteFunction(delFunc);
+            System.out.println("The Lambda function is deleted");
+
+        } catch (ServiceException e) {
+            System.out.println("ServiceException: " + e);
+        } catch (AmazonServiceException ase) {
+            System.out.println("Caught an AmazonServiceException, " +
+                    "which means your request made it " +
+                    "to AWS Lambda, but was rejected with an error " +
+                    "response for some reason.");
+            System.out.println("Error Message:    " + ase.getMessage());
+            System.out.println("HTTP Status Code: " + ase.getStatusCode());
+            System.out.println("AWS Error Code:   " + ase.getErrorCode());
+            System.out.println("Error Type:       " + ase.getErrorType());
+            System.out.println("Request ID:       " + ase.getRequestId());
+        } catch (AmazonClientException ace) {
+            System.out.println("Caught an AmazonClientException, " +
+                    "which means the client encountered " +
+                    "an internal error while trying to " +
+                    " communicate with AWS Lambda, " +
+                    "such as not being able to access the network.");
+            System.out.println("Error Message: " + ace.getMessage());
+        }
+    }
+}
