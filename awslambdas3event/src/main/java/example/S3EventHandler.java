a2d07616d9828312618dc4a8a2fa78e137a6b79b
alfonsof <alfonsofb@gmail.com>
2021-03-16 23:54:39 +0100

    Refactoring

--- awslambdas3event/src/main/java/example/S3EventHandler.java
+++ awslambdas3event/src/main/java/example/S3EventHandler.java
@@ -3,26 +3,28 @@
  * It handles an AWS simple Lambda function that sends information to the log
  * about an object when it appears in a S3 bucket.
  */
- 
+
 package example;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.s3.event.S3EventNotification.S3EventNotificationRecord;
+import com.amazonaws.services.lambda.runtime.events.S3Event;
+import com.amazonaws.services.lambda.runtime.events.models.s3.S3EventNotification.S3EventNotificationRecord;
 
-public class S3CreationEvent implements RequestHandler<com.amazonaws.services.lambda.runtime.events.S3Event, String> {
+public class S3EventHandler implements RequestHandler<S3Event, String> {
 
-    public String handleRequest(com.amazonaws.services.lambda.runtime.events.S3Event input, Context context) {
+    @Override
+    public String handleRequest(S3Event s3Event, Context context) {
         // Get Event Record
-        S3EventNotificationRecord record = input.getRecords().get(0);
+        S3EventNotificationRecord record = s3Event.getRecords().get(0);
 
         // Source Bucket Name
         String srcBucketName = record.getS3().getBucket().getName();
 
         // Source File Name
-        String srcObjectName = record.getS3().getObject().getKey(); // Name doesn't contain any special characters
+        String srcObjectName = record.getS3().getObject().getUrlDecodedKey();
 
-        context.getLogger().log("Input:  " + input + "\n");
+        context.getLogger().log("S3Event: " + s3Event + "\n");
         context.getLogger().log("Bucket: " + srcBucketName + "\n");
         context.getLogger().log("Object: " + srcObjectName + "\n");
 
